64d49cfbb90ec164d6d5042dfe17597fc2bd6c8b
jackshelloworld <65469145+jackshelloworld@users.noreply.github.com>
2022-05-19 21:46:41 -0600

    asdf

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -17,13 +17,13 @@ const NotFound = () => (
       <div>
         <Divider speed={0.2} offset={0} factor={1}>
           <UpDown>
-            <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
-            <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
+            <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_grey" left="10%" top="20%" />
+            <Svg icon="hexa" width={48} stroke color="icon_grey" left="60%" top="70%" />
             <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" />
           </UpDown>
           <UpDownWide>
-            <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
-            <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+            <Svg icon="arrowUp" hiddenMobile width={16} color="icon_grey" left="80%" top="10%" />
+            <Svg icon="triangle" width={12} stroke color="icon_grey" left="90%" top="50%" />
             <Svg icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
             <Svg icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
             <Svg icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
